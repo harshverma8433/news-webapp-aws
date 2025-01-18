d858ff63ef540878147d8eabb51a754813c7b2d2
harsh verma <harsh>
2025-01-19 00:35:43 +0530

    app.js changes

--- server/app.js
+++ server/app.js
@@ -28,6 +28,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT;
+console.log(PORT);
+
 connectDataBase().then(() => {
   app.listen(PORT, () => {
     console.log("server start on port" , process.env.PORT);
